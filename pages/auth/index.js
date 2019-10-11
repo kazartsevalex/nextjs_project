4a31b42565942668896e145002e1dcfee27cbe03
Alex K <damned182@mail.ru>
2019-10-11 20:31:39 +0300

    add simple React component

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Router from 'next/router';
 
+import User from '../../components/User';
+
 const authIndexPage = () => (
   <div>
     <h1>The Auth Page</h1>
+    <User name="Adam" age="999" />
     <p>
       <button onClick={() => Router.push('/')}>Go to Main page</button>
     </p>
