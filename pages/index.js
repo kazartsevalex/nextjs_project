ef7e3fc1a2c1e4ac4b7d790ba4a093af9b63ff7b
Alex K <damned182@mail.ru>
2019-10-11 20:55:52 +0300

    getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
 
-const indexPage = () => (
-  <div>
-    <h1>The Main Page</h1>
-    <p>
-      Go to <Link href="/auth"><a>Auth</a></Link>
-    </p>
-  </div>
-);
+class IndexPage extends React.Component {
+  static getInitialProps(context) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ appName: 'Super App!' })
+      }, 1000);
+    });
 
-export default indexPage;
+    return promise;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The Main Page of {this.props.appName}</h1>
+        <p>
+          Go to <Link href="/auth"><a>Auth</a></Link>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
